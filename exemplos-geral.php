9b5c78696cc004bd5075ab80232026d57d2f90db
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-06-06 11:36:50 -0300

    Criando o index e objetos

--- exemplos-geral.php
+++ exemplos-geral.php
@@ -5,13 +5,16 @@ class Exemplo {
     // Atributos ou Propriedades
     public string $nome;
     public string $escola;
+    public string $cidade;
 }
 
 // Criar objetos/instâncias
 $exemploA = new Exemplo;
+$exemploB = new Exemplo;
 
 echo "<pre>";
 var_dump($exemploA);
+var_dump($exemploB);
 echo "</pre>";
 
 
---
+++ src/Cliente.php
@@ -0,0 +1,8 @@
+<?php
+class Cliente {
+    // Propriedades (ou atributos)
+    public string $nome;
+    public string $email;
+    public string $senha;
+    public array $telefones;
+}
