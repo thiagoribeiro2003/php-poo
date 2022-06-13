3831a67adad9bf6e866d9171a3a6b3d62e97f5da
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-06-13 10:54:21 -0300

    alterando title e h1 para a branch 6

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 5</title>
+    <title>Exemplo 6</title>
 </head>
 <body>
-    <h1>PHP POO - Exemplo 5</h1> 
+    <h1>PHP POO - Exemplo 6</h1> 
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
